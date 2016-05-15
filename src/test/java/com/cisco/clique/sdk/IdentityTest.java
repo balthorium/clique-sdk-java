b3bd8f3dce24d1acc4cefbddeea95803d7a1eebf
Andrew Biggs (adb) <adb@cisco.com>
2016-05-14 19:36:44 -0600

    Get gradle configured for testng.

---
+++ src/test/java/com/cisco/clique/sdk/IdentityTest.java
@@ -0,0 +1,16 @@
+package com.cisco.clique.sdk;
+
+import org.testng.annotations.*;
+
+public class IdentityTest {
+
+    @BeforeClass
+    public void setUp() {
+        System.out.println("setups!");
+    }
+
+    @Test
+    public void aFastTest() {
+        System.out.println("Faster test");
+    }
+}
